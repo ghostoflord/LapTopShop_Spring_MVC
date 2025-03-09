d11e4b71be913ad81722b365328055d8c300e564
ghostoflord <ghost2892003zzz@gmail.com>
2025-03-09 09:58:49 +0700

    update config code

--- src/main/java/vn/teamwork/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/teamwork/laptopshop/LaptopshopApplication.java
@@ -3,10 +3,10 @@ package vn.teamwork.laptopshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
+// @SpringBootApplication(exclude = {
+// 		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
 
-// @SpringBootApplication
+@SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
